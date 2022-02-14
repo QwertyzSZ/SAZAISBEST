4b7d62517927de5597aed858d0b56c6f6b5ef822
QwertyzSZ <98988528+QwertyzSZ@users.noreply.github.com>
2022-02-14 13:31:41 +0700

    Create mainloadbf.lua

---
+++ GameSupport/mainloadbf.lua
@@ -0,0 +1,5 @@
+if not game:GetService("UserInputService").TouchEnabled and not game:GetService("UserInputService").KeyboardEnabled == false then
+  loadstring(game:HttpGet"hhttps://raw.githubusercontent.com/QwertyzSZ/SAZAISBEST/main/GameSupport/BloxFruitSZ.lua")()
+  else
+  loadstring(game:HttpGet"https://raw.githubusercontent.com/QwertyzSZ/SAZAISBEST/main/GameSupport/BFMobile.lua")()
+ end
