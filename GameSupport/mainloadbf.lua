5e5c08f5a4a009663367bb550e0ffa4f89c36d99
QwertyzSZ <98988528+QwertyzSZ@users.noreply.github.com>
2022-02-14 14:26:52 +0700

    Update mainloadbf.lua

--- GameSupport/mainloadbf.lua
+++ GameSupport/mainloadbf.lua
@@ -1,5 +1,5 @@
-if not game:GetService("UserInputService").TouchEnabled and not game:GetService("UserInputService").KeyboardEnabled == false then
-  loadstring(game:HttpGet"hhttps://raw.githubusercontent.com/QwertyzSZ/SAZAISBEST/main/GameSupport/BloxFruitSZ.lua")()
-  else
-  loadstring(game:HttpGet"https://raw.githubusercontent.com/QwertyzSZ/SAZAISBEST/main/GameSupport/BFMobile.lua")()
- end
+    if not game:GetService("UserInputService").TouchEnabled and not game:GetService("UserInputService").KeyboardEnabled == false then
+        loadstring(game:HttpGet"https://raw.githubusercontent.com/QwertyzSZ/SAZAISBEST/main/GameSupport/BloxFruitSZ.lua")()
+        else
+        loadstring(game:HttpGet"https://raw.githubusercontent.com/QwertyzSZ/SAZAISBEST/main/GameSupport/BFMobile.lua")()
+       end
