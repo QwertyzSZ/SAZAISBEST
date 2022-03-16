4515a4700c621810c848ddf5fbc489f5d07fec52
QwertyzSZ <98988528+QwertyzSZ@users.noreply.github.com>
2022-03-16 10:56:55 +0700

    Update PremiumLoad.lua

--- PremiumLoad.lua
+++ PremiumLoad.lua
@@ -1,7 +1,7 @@
 local PlaceId = game.PlaceId
 
 if PlaceId == 6284583030 or PlaceId == 7722306047 then
-        loadstring(game:HttpGet"https://raw.githubusercontent.com/QwertyzSZ/SAZAISBEST/main/PremiumGame/PremiumPsxx.lua")()
+        loadstring(game:HttpGet"https://raw.githubusercontent.com/QwertyzSZ/SAZAISBEST/main/PremiumGame/psxPremium.lua")()
 else
 	game.Players.LocalPlayer:kick("Wrong Game!")
 	wait(1)
