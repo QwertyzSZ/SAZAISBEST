cceb7485bb487c8dc04d831b0ee4d3ac3b1d1e23
QwertyzSZ <98988528+QwertyzSZ@users.noreply.github.com>
2022-04-03 17:04:58 +0700

    Create second.lua

---
+++ second.lua
@@ -0,0 +1,10 @@
+_G.Keys = _G.wl_key
+local PlaceId = game.PlaceId
+
+if PlaceId == 6284583030 or PlaceId == 7722306047 then
+        loadstring(game:HttpGet"https://raw.githubusercontent.com/QwertyzSZ/SAZAISBEST/main/PremiumGame/psxPremium.lua")()
+else
+	game.Players.LocalPlayer:kick("Wrong Game!")
+	wait(1)
+	game:Shutdown()
+end
