40cacaeb20c0f40f4fb1e6c0ebdab56e0161e0a1
QwertyzSZ <98988528+QwertyzSZ@users.noreply.github.com>
2022-04-19 12:08:51 +0700

    Create version.lua

---
+++ version.lua
@@ -0,0 +1,6 @@
+getgenv().Version = "<font color='rgb(0, 0, 255)'><b>1.5.0</b></font><font size ='11' color='rgb(255,0,0)'> a</font>"
+local Ui = game:GetService("CoreGui"):FindFirstChild(getgenv().Version)
+if  Ui then
+   Ui:Destroy()
+end
+ 
