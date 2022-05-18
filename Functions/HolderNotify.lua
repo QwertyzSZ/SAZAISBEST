ffe9f83125672cf6362bebe650942512a23c27cc
QwertyzSZ <98988528+QwertyzSZ@users.noreply.github.com>
2022-05-18 13:16:04 +0700

    Create HolderNotify.lua

---
+++ Functions/HolderNotify.lua
@@ -0,0 +1,9 @@
+local GUI = game:GetService("CoreGui"):FindFirstChild("MainNotify")
+if not GUI then
+    local MainNotify = instance.new("ScreenGui")
+    MainNotify.Name = "MainNotify"
+    MainNotify.Parent = game:GetService("CoreGui")
+    MainNotify.ZIndexBehavior = Enum.ZIndexBehavior.Global
+    MainNotify.ResetOnSpawn = false
+  else
+end
