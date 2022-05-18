97f693ed5f801ad7c87b8f79ae0aef3a69e96a04
QwertyzSZ <98988528+QwertyzSZ@users.noreply.github.com>
2022-05-18 13:21:45 +0700

    Update and rename HolderNotify.lua to HolderNotifyy.lua

--- Functions/HolderNotifyy.lua
+++ Functions/HolderNotifyy.lua
@@ -1,6 +1,6 @@
 local GUI = game:GetService("CoreGui"):FindFirstChild("MainNotify")
 if not GUI then
-    local MainNotify = instance.new("ScreenGui")
+    local MainNotify = Instance.new("ScreenGui")
     MainNotify.Name = "MainNotify"
     MainNotify.Parent = game:GetService("CoreGui")
     MainNotify.ZIndexBehavior = Enum.ZIndexBehavior.Global
